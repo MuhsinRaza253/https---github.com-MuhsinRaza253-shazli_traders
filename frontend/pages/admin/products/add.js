2f3ae8fe13cd2ddffc3900ee4b109618e153ffca
Muhsin Raza <muhsin.raza@scalateams.com>
2026-05-07 10:24:55 +0500

    fix: import Link in admin product add page

--- frontend/pages/admin/products/add.js
+++ frontend/pages/admin/products/add.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import toast from 'react-hot-toast';
